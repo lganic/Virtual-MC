b6e7265ab9556a345db43984f1e30375c1cd02d9
lganic <55366941+lganic@users.noreply.github.com>
2025-01-26 18:17:36 -0500

    added doc strings to fixed point conversions

--- src/virtual_mc/data/fixed_point.py
+++ src/virtual_mc/data/fixed_point.py
@@ -1,8 +1,16 @@
 def convert_to_fixed_point(floating_point_num: float, alignment: int) -> int:
 
+    '''
+    Convert a floating point number to a fixed point binary integer
+    '''
+
     return int(floating_point_num * (1 << alignment))
 
 def convert_from_fixed_point(fixed_point_num: int, alignment: int) -> float:
+
+    '''
+    Convert a fixed point binary integer to a floating point number
+    '''
 
     return fixed_point_num / (1 << alignment)
     
