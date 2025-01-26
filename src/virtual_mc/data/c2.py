2e3e2986f940cf25348e9161853b7ac8fb0fa08c
lganic <55366941+lganic@users.noreply.github.com>
2025-01-26 17:36:53 -0500

    added from 2s complement conversion

--- src/virtual_mc/data/c2.py
+++ src/virtual_mc/data/c2.py
@@ -8,3 +8,20 @@ def convert_to_2s_complement(source_number: int, num_bits: int) -> int:
     inverted_bits = inverter_num - abs(source_number)
 
     return inverted_bits + 1
+
+def convert_from_2s_complement(converted_number: int, num_bits: int):
+
+    check_sign_flag_num = 2 ** (num_bits - 1)
+
+    if converted_number & check_sign_flag_num == 0:
+        # Number is positive, no conversion needed
+        return converted_number
+
+    inverter_num = (2 ** num_bits) - 1
+
+    inverted_bits = converted_number - 1
+
+    source_number = inverter_num - inverted_bits
+
+    return -source_number # Re-invert number
+
