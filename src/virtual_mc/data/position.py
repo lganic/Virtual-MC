d77eb0e809e35a0d7467c9bf3c12b2608f436a74
lganic <55366941+lganic@users.noreply.github.com>
2025-01-26 17:54:26 -0500

    Added position conversion

---
+++ src/virtual_mc/data/position.py
@@ -0,0 +1,31 @@
+from typing import Union, TypeAlias
+from ..exceptions import check_range
+from .c2 import convert_to_2s_complement
+
+Number: TypeAlias = Union[float, int]
+
+def convert_position_to_long(x: Number, y: Number, z:Number) -> int:
+
+    # Round out fields to int
+    x = round(x)
+    y = round(y)
+    z = round(z)
+
+    max_26 = 33554431 # 2^25-1
+    min_26 = -33554432 # -2^25
+
+    max_12 = 2047 # 2^11-1
+    min_12 = -2048 # 2^11
+
+    # Ensure that x, y, and z are within valid ranges.
+    check_range(x, min_26, max_26, name = 'X Coordinate')
+    check_range(y, min_12, max_12, name = 'Y Coordinate')
+    check_range(z, min_26, max_26, name = 'Z Coordinate')
+
+    # Convert to 2s complement
+    x_c = convert_to_2s_complement(x, 26)
+    y_c = convert_to_2s_complement(y, 12)
+    z_c = convert_to_2s_complement(z, 26)
+
+    # Assemble final long
+    return (x_c << 36) + (z_c << 36) + y_c
---
+++ src/virtual_mc/exceptions.py
@@ -0,0 +1,13 @@
+class OutOfRange(Exception):
+    pass
+
+def check_range(value: int, minimum_valid: int, maximum_valid: int, name = 'Value'):
+    '''
+    Check the range of a given variable, and ensure that it within the specified (inclusive) range
+    '''
+
+    if value > maximum_valid:
+        raise OutOfRange(f'{name} is greater than allowed limit: {value} > {maximum_valid}')
+    
+    if value < minimum_valid:
+        raise OutOfRange(f'{name} is less than the allowed limit: {value} < {minimum_valid}')
