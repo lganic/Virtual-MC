50f35e60a3fea03bb77aef1f7e9132e5480f7acc
lganic <55366941+lganic@users.noreply.github.com>
2025-01-27 18:38:39 -0500

    added id set type

---
+++ src/virtual_mc/data/types/id_set.py
@@ -0,0 +1,36 @@
+from .generic import Byteable_Object
+from .string import String
+from .array import Array
+from ..var_int import write_var_int
+from .numbers import VarInt
+
+class IdSet(Byteable_Object):
+
+    def __init__(self, type: int, tag_name: None, ids = []):
+
+        self.type = type
+        self.tag_name = tag_name
+        self.ids = ids
+    
+    def to_bytes(self):
+        
+        type_bytes = write_var_int(self.type)
+
+        if self.type == 0:
+
+            # Return identifier
+
+            tag_object = String(self.tag_name)
+
+            return type_bytes + tag_object.to_bytes()
+        
+        if len(self.ids) != self.type - 1:
+            raise ValueError('Size mismatch, id array length must be equal to type - 1')
+    
+        id_array = Array()
+
+        for id in self.ids:
+
+            id_array.add_object(VarInt(id))
+        
+        return type_bytes + id_array.to_bytes()
