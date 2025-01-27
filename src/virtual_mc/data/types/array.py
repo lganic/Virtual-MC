99983fa1deb585980c5bf0d621af34414e9e08a1
lganic <55366941+lganic@users.noreply.github.com>
2025-01-26 21:19:51 -0500

    Created prefixed array

--- src/virtual_mc/data/types/array.py
+++ src/virtual_mc/data/types/array.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from .generic import Byteable_Object
+from ..var_int import write_var_int
 
 class Array(Byteable_Object):
 
@@ -18,3 +19,15 @@ class Array(Byteable_Object):
     def add_object(self, obj: Byteable_Object):
 
         self.objects.append(obj)
+
+class PrefixedArray(Array):
+    
+    def to_bytes(self):
+
+        # Fetch array bytes
+        base_output = super().to_bytes()
+
+        # Add varint length to packet
+        length_varint = write_var_int(len(base_output))
+
+        return length_varint + base_output
