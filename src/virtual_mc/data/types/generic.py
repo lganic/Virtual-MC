74692ecb1b151f0a90cf11ed587022647fc9a580
lganic <55366941+lganic@users.noreply.github.com>
2025-01-26 21:15:46 -0500

    added type hinting to generic TCP object

--- src/virtual_mc/data/types/generic.py
+++ src/virtual_mc/data/types/generic.py
@@ -3,7 +3,11 @@
 Generic definition of object which can be transmitted over the TCP connection
 '''
 
+
 class Byteable_Object:
     
-    def to_bytes(self):
+    def to_bytes(self) -> bytes:
+
+        '''Return the content of the object as a byte string'''
+
         raise NotImplementedError('This functionality is not implemented on this object type!')
