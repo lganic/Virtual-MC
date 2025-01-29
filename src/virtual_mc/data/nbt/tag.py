bbdd6927b78692258008ecd356c157e7bf15b0ff
Logan Boehm <sidwirb@gmail.com>
2025-01-29 17:26:50 -0500

    Added parse buffer implementation to generic nbt tag type

--- src/virtual_mc/data/nbt/tag.py
+++ src/virtual_mc/data/nbt/tag.py
@@ -3,6 +3,7 @@
 Generic definition of NBT tag object
 '''
 
+from typing import Tuple, Any
 from .nbt_util import encode_short
 
 class NBT_Tag:
@@ -29,3 +30,20 @@ class NBT_Tag:
             base += encode_short(len(name_bytes)) + name_bytes
         
         return base + self.payload()
+    
+    def parse_buffer(self, buffer: bytes, index: int, ignore_name: bool = False) -> Tuple[Any, int]:
+        """
+        Parses an object from a bytes buffer starting at a specified index.
+
+        Args:
+            buffer (bytes): The bytes buffer containing the data to parse.
+            index (int): The starting index within the buffer from which to begin parsing.
+            ignore_name (bool, optional): If set to `True`, the parser will ignore the object's name
+                                        during parsing. Defaults to `False`.
+
+        Returns:
+            Tuple[Any, int]: A tuple where the first element is the parsed object and the second
+                            element is the number of bytes that were parsed.
+        """
+
+        raise NotImplementedError('This object has not implemented this function')
