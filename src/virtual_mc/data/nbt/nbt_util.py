b36e0733ee25996ea931b014840cf63885e11d95
Logan Boehm <sidwirb@gmail.com>
2025-01-29 17:38:35 -0500

    added decode utils to nbt utils

--- src/virtual_mc/data/nbt/nbt_util.py
+++ src/virtual_mc/data/nbt/nbt_util.py
@@ -5,3 +5,18 @@ def encode_short(n: int):
 def encode_int(n: int):
 
     return n.to_bytes(4, 'big')
+
+def decode_short(b: bytes) -> int:
+
+    if len(b) != 2:
+        raise ValueError(f"decode_short expects 2 bytes, got {len(b)} bytes.")
+
+    return int.from_bytes(b, 'big')
+
+
+def decode_int(b: bytes) -> int:
+
+    if len(b) != 4:
+        raise ValueError(f"decode_int expects 4 bytes, got {len(b)} bytes.")
+
+    return int.from_bytes(b, 'big')
