91ff6ccb1c177d88f8bfcd0adfb78ff38160be95
lganic <55366941+lganic@users.noreply.github.com>
2025-01-29 21:29:12 -0500

    Added NBT file test cases

--- tests/data/nbt/test_compound_encoding.py
+++ tests/data/nbt/test_compound_encoding.py
@@ -1,5 +1,7 @@
 import pytest
+import os
 from virtual_mc.data.nbt.nbt_types import NBT_Compound, NBT_String
+import gzip
 
 
 def test_bananarama():
@@ -18,7 +20,42 @@ def test_bananarama():
 
     expected = bytes([0x0a,0x00,0x0b,0x68,0x65,0x6c,0x6c,0x6f,0x20,0x77,0x6f,0x72,0x6c,0x64,0x08,0x00,0x04,0x6e ,0x61,0x6d,0x65,0x00,0x09,0x42,0x61,0x6e,0x61,0x6e,0x72,0x61,0x6d,0x61,0x00])
 
-    print('Result:   ', result)
-    print('Expected: ', expected)
-
     assert result == expected
+
+def test_hello_world_nbt():
+
+    # Load hello world NBT
+
+    with open(os.path.join(os.path.dirname(__file__), 'hello_world.nbt') , 'rb') as f:
+        contents = f.read()
+    
+    result = name, object, size = NBT_Compound.parse_buffer(contents, 0)
+
+    object: NBT_Compound
+
+    assert object.pretty_tree() == "NBT_Compound('hello world'): 1 entry\n{\n\tNBT_String('name'): Bananrama\n}\n"
+
+    recoded = object.to_bytes()
+
+    assert object.is_network == False
+
+    assert contents == recoded
+
+def test_bigtest_nbt():
+
+    # Load hello world NBT
+
+    with open(os.path.join(os.path.dirname(__file__), 'bigtest.nbt') , 'rb') as f:
+        contents = f.read()
+
+    contents = gzip.decompress(contents)
+
+    result = name, object, size = NBT_Compound.parse_buffer(contents, 0)
+
+    object: NBT_Compound
+
+    recoded = object.to_bytes()
+
+    assert object.is_network == False
+
+    assert contents == recoded
