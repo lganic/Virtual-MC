33754f850e6a97b5ca4f7fc886d85490c2627e2b
lganic <me@logan-boehm.com>
2025-05-14 11:02:43 -0400

    Refactored version data to use json instead of txt

--- src/virtual_mc/unpacking/build_all.py
+++ src/virtual_mc/unpacking/build_all.py
@@ -1,9 +1,11 @@
 import os
+import json
+
 from .server_unpacker import cleanup, unpack_server, get_most_recent_version, server_abspath
 from .build_block_state_ids import build_id_lookup
 from .read_version_json import read_version_json
 
-VERSION_PATH = os.path.join(os.path.dirname(__file__), '../../../data/server_version.txt')
+VERSION_PATH = os.path.join(os.path.dirname(__file__), '../../../data/server_version.json')
 
 def build_all():
 
@@ -16,11 +18,16 @@ def build_all():
 
     cleanup()
 
-    print('Updating server_version.txt')
+    print('Updating server_version.json')
     most_recent_version = get_most_recent_version()
 
+    digest = {
+        'server_version': most_recent_version,
+        'protocol_version': server_protocol_version
+    }
+
     with open(VERSION_PATH, 'w') as f:
-        f.write(most_recent_version)
+        json.dump(digest, f)
 
     print('Done!')
 
