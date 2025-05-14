4eb35549346391c75a0e6b7e290ecb5f2da21e00
lganic <me@logan-boehm.com>
2025-05-14 12:28:32 -0400

    Added next state checking to handshake parsing.

--- src/virtual_mc/communication/handshake.py
+++ src/virtual_mc/communication/handshake.py
@@ -6,6 +6,10 @@ from .utilities import get_server_protocol_version
 # Cache this value, so we don't constantly have to fetch it from disk
 SERVER_PROTOCOL_VERSION = get_server_protocol_version()
 
+STATUS = 1
+LOGIN = 2
+TRANSFER = 3
+
 def parse_handshake(bytes):
 
     assert bytes[0] == 0
@@ -23,5 +27,8 @@ def parse_handshake(bytes):
     bytes = bytes[2:]
 
     next_state = bytes[0] # This is technically a varint, but the values are never big enough to need more than 1 byte
+
+    if next_state > 3:
+        raise ValueError(f"Next state is invalid. Current accepted types are: Status(1), Login(2), Transfer(3). Got a value of: {next_state}")
 
     return protocol_version, server_address, server_port, next_state
