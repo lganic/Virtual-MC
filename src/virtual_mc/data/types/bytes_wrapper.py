fbc38025a6a4d38d194baa2d32d5958dded497c7
Logan Boehm <sidwirb@gmail.com>
2025-08-27 18:14:12 -0400

    Added varint length to byte wrapper

--- src/virtual_mc/data/types/bytes_wrapper.py
+++ src/virtual_mc/data/types/bytes_wrapper.py
@@ -1,8 +1,9 @@
 from .generic import Byteable_Object
+from ..varint import write_var_int_bytes
 
 # Wrapper class to just make notation easier. 
 class Bytes:
     def __init__(self, payload: bytes):
         self.data = payload
     def to_bytes(self):
-        return self.data
+        return write_var_int_bytes(len(self.data)) + self.data
